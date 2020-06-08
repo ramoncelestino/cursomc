c13a4fc567e69602e8f24c9685c682e13ee9326f
Ramon Celestino <ramoncelesteramos@gmail.com>
2020-06-08 14:53:48 -0300

    Criando primeira classe dominio - Categoria

--- src/main/java/com/ramon/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/ramon/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,31 @@
 package com.ramon.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ramon.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> categorias = new ArrayList<Categoria>();
+		
+		categorias.add(cat1);
+		categorias.add(cat2);
+		
+		return categorias;
+		
+		
 	}
 
 }
